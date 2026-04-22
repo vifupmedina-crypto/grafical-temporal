db9b92dcaf7ca006a4b9aee7452fa280b2ee1cf8
vifupmedina-crypto <vifup.medina@gmail.com>
2026-04-22 14:26:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ with st.sidebar:
 
     st.markdown("### 📅 Rango de años")
     col1, col2 = st.columns(2)
-    yr_from = col1.number_input("Desde", value=1680, step=10)
-    yr_to   = col2.number_input("Hasta", value=1850, step=10)
+    yr_from = col1.number_input("Desde", value=1800, step=10)
+    yr_to   = col2.number_input("Hasta", value=1880, step=10)
 
     st.markdown("### 👤 Personajes")
     col_a, col_b = st.columns(2)
@@ -123,8 +123,8 @@ with st.sidebar:
     with st.expander("➕ Añadir personaje"):
         np_nombre = st.text_input("Nombre", key="np_nombre")
         c1, c2 = st.columns(2)
-        np_nac  = c1.number_input("Año nacimiento", value=1700, step=1, key="np_nac")
-        np_muer = c2.number_input("Año muerte",     value=1780, step=1, key="np_muer")
+        np_nac  = c1.number_input("Año nacimiento", value=1800, step=1, key="np_nac")
+        np_muer = c2.number_input("Año muerte",     value=1880, step=1, key="np_muer")
         np_gen  = st.radio("Género", ["♂ Hombre", "♀ Mujer"], horizontal=True, key="np_gen")
         c3, c4  = st.columns(2)
         np_nac_ap  = c3.checkbox("Nac. aprox. (?)", key="np_nac_ap")
